f7e6f92ccd8f5554e0a6b89e468c6cbda6fb936d
NeiltonSeguins <seguins.neilton@gmailcom>
2025-04-07 16:19:31 -0300

    Código final da aula

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
-let darkmode = null;
+let darkmode = localStorage.getItem("darkmode");
 const themeSwitch = document.getElementById("theme-toggle");
 
 const enableDarkmode = () => {
   document.body.classList.add("darkmode");
+  localStorage.setItem("darkmode", "active");
 };
 
 const disableDarkmode = () => {
   document.body.classList.remove("darkmode");
+  localStorage.setItem("darkmode", null);
 };
 
 if (darkmode === "active") enableDarkmode();
 
 themeSwitch.addEventListener("click", () => {
+  darkmode = localStorage.getItem("darkmode");
   darkmode !== "active" ? enableDarkmode() : disableDarkmode();
+});
+
+window.addEventListener("storage", (event) => {
+  if (event.key === "darkmode") {
+    const darkmode = localStorage.getItem("darkmode");
+    if (darkmode === "active") {
+      enableDarkmode();
+    } else {
+      disableDarkmode();
+    }
+  }
 });
